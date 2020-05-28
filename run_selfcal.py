c2769e03c12cd04e34bcc5694f37c63cc80fdce4
Xiang Zhang <zhangxiang_planet@outlook.com>
2020-05-28 23:44:09 +0800

    Update: change selfcal masking level

--- run_selfcal.py
+++ run_selfcal.py
@@ -26,13 +26,15 @@ def get_noise(img_name):
 # 	mask = mask.astype(bool)
 # 	rms = np.std(data[mask])
 	rms_initial = np.std(data)
-	rms = np.std(data[data<2.5*rms_initial])
-	peak = np.amax(data)
+	rms = np.std(data[np.logical_and(data>-2.5*rms_initial, data<2.5*rms_initial)])
+	peak_max = np.amax(data)
+	peak_min = np.amin(data)
 	hdu.close()
-	return rms,peak
+	return rms,peak_max,peak_min
 
 sourcename = sys.argv[1]
 vislist = [sourcename+'.2100', sourcename+'.5500', sourcename+'.7500']
+
 logf = open(sourcename+'.scal.log','w',1)
 
 for t in vislist:
@@ -48,47 +50,53 @@ for t in vislist:
 	logprint("***** Start selfcal: %s *****"%t, logf)
 	logprint("Generate the dirty image:", logf)
 	# Generate a MFS image without selfcal.
-	call(['invert', 'vis=%s'%t_pscal, 'map=%s'%t_map, 'beam=%s'%t_beam, 'robust=0.5', 'stokes=i', 'options=mfs,double,sdb', 'imsize=3,3,beam', 'cell=5,5,res'],stdout=logf,stderr=logf)
+	call(['invert', 'vis=%s'%t_pscal, 'map=%s'%t_map, 'beam=%s'%t_beam, 'robust=0.5', 'stokes=i', 'options=mfs,double,sdb', 'imsize=2,2,beam', 'cell=5,5,res'],stdout=logf,stderr=logf)
 	call(['fits', 'op=xyout', 'in=%s'%t_map, 'out=%s'%t_dirty],stdout=logf,stderr=logf)
-	sigma, peak_flux = get_noise(t_dirty)
-	sigma10 = 10.0*sigma
-	sigma5 = 5.0*sigma
+	sigma, peak_max, peak_min = get_noise(t_dirty)
+	# sigma5 = 0.0005
+	# sigma2 = 0.0002
+
+	clean_level = 5.0*sigma
 	
 	logprint("RMS of dirty image: %s"%sigma, logf)
-	logprint("Peak flux density of dirty image: %s"%peak_flux, logf)
-	logprint("Generate a shallow cleaned image:", logf)
-	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(sigma10, sigma5), "region='perc(90)'"],stdout=logf,stderr=logf)
+	# logprint("Peak flux density of dirty image: %s"%peak_max, logf)
+	logprint("Generate a cleaned image:", logf)
+	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(clean_level, 2*sigma), "region='perc(90)'"],stdout=logf,stderr=logf)
 	call(['restor', 'map=%s'%t_map, 'beam=%s'%t_beam, 'model=%s'%t_model, 'out=%s'%t_restor],stdout=logf,stderr=logf)
 	call(['fits', 'op=xyout', 'in=%s'%t_restor, 'out=%s'%t_p0],stdout=logf,stderr=logf)
-	sigma, peak_flux = get_noise(t_p0)
-	dynamic_range = peak_flux/sigma
-	logprint("RMS of p0 image: %s"%sigma, logf)
-	logprint("Peak flux density of p0 image: %s"%peak_flux, logf)
-	if dynamic_range >= 100:
-		logprint("Wow this is a bright source!", logf)
-		bool_bright = 1
-	elif 100 > dynamic_range >= 20:
-		logprint("This is a moderate source.", logf)
-		bool_bright = 0
-	else:
-		logprint("This is a faint source. Selfcal might not be a good idea...", logf)
-		exit(1)
+	sigma, peak_max, peak_min = get_noise(t_p0)
+	# dynamic_range = peak_flux/sigma
+	# logprint("RMS of p0 image: %s"%sigma, logf)
+	# logprint("Peak flux density of p0 image: %s"%peak_flux, logf)
+	# if dynamic_range >= 100:
+	# 	logprint("Wow this is a bright source!", logf)
+	# 	bool_bright = 1
+	# elif 100 > dynamic_range >= 20:
+	# 	logprint("This is a moderate source.", logf)
+	# 	bool_bright = 0
+	# else:
+	# 	logprint("This is a faint source. Selfcal might not be a good idea...", logf)
+	# 	exit(1)
 	
 	# First round of phase selfcal.
 	# Generate a mask
 	logprint("***** First round of phase selfcal *****", logf)
-	sigma5 = 5.0*sigma
-	if bool_bright == True:
-		mask_level = np.amax([50.0*sigma, 0.4*peak_flux])
-		clean_level = 20.0*sigma
-	else:
-		mask_level = np.amax([20.0*sigma, 0.4*peak_flux])
-		clean_level = 10.0*sigma
+	# sigma2 = 2.0*sigma
+	# if bool_bright == True:
+	# 	# mask_level = np.amax([50.0*sigma, 0.4*peak_flux])
+	# 	mask_level = 10.0*sigma
+	# 	clean_level = 3.0*sigma
+	# else:
+	# 	# mask_level = np.amax([20.0*sigma, 0.4*peak_flux])
+	# 	mask_level = 10.0*sigma
+	# 	clean_level = 3.0*sigma
+	mask_level = np.amax([10*sigma, -peak_min*1.5])
+	clean_level = 5.0*sigma
 	call(['maths', 'exp=<%s>'%t_restor, 'mask=<%s>.gt.%s'%(t_restor,mask_level), 'out=%s'%t_mask],stdout=logf,stderr=logf)
 	shutil.rmtree(t_restor)
 	shutil.rmtree(t_model)
-	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(clean_level, sigma5), 'region=mask(%s)'%t_mask],stdout=logf,stderr=logf)
-	call(['selfcal', 'vis=%s'%t_pscal, 'model=%s'%t_model, 'interval=5', 'nfbin=2', 'options=phase,mfs'],stdout=logf,stderr=logf)
+	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=1500', 'cutoff=%s,%s'%(clean_level, 2*sigma), 'region=mask(%s)'%t_mask],stdout=logf,stderr=logf)
+	call(['selfcal', 'vis=%s'%t_pscal, 'model=%s'%t_model, 'interval=5', 'nfbin=1', 'options=phase,mfs'],stdout=logf,stderr=logf)
 	shutil.rmtree(t_map)
 	shutil.rmtree(t_beam)
 	shutil.rmtree(t_mask)
@@ -96,74 +104,74 @@ for t in vislist:
 	# os.remove(t_dirty)
 
 	t_p1 = t + '.p1.fits'
-	call(['invert', 'vis=%s'%t_pscal, 'map=%s'%t_map, 'beam=%s'%t_beam, 'robust=0.5', 'stokes=i', 'options=mfs,double,sdb', 'imsize=3,3,beam', 'cell=5,5,res'],stdout=logf,stderr=logf)
-	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(clean_level, sigma5), "region='perc(90)'"],stdout=logf,stderr=logf)
+	call(['invert', 'vis=%s'%t_pscal, 'map=%s'%t_map, 'beam=%s'%t_beam, 'robust=0.5', 'stokes=i', 'options=mfs,double,sdb', 'imsize=2,2,beam', 'cell=5,5,res'],stdout=logf,stderr=logf)
+	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(clean_level, 2*sigma), "region='perc(90)'"],stdout=logf,stderr=logf)
 	call(['restor', 'map=%s'%t_map, 'beam=%s'%t_beam, 'model=%s'%t_model, 'out=%s'%t_restor],stdout=logf,stderr=logf)
 	call(['fits', 'op=xyout', 'in=%s'%t_restor, 'out=%s'%t_p1],stdout=logf,stderr=logf)
-	sigma, peak_flux = get_noise(t_p1)
-	logprint("RMS of p1 image: %s"%sigma, logf)
-	logprint("Peak flux density of p1 image: %s"%peak_flux, logf)
+	sigma, peak_max, peak_min = get_noise(t_p1)
+	# logprint("RMS of p1 image: %s"%sigma, logf)
+	# logprint("Peak flux density of p1 image: %s"%peak_flux, logf)
 
 	# Second round.
 	logprint("***** Second round of phase selfcal *****", logf)
-	sigma5 = 5.0*sigma
-	if bool_bright == True:
-		mask_level = 50.0*sigma
-		clean_level = 20.0*sigma
-	else:
-		mask_level = 20.0*sigma
-		clean_level = 10.0*sigma
+	mask_level = np.amax([10*sigma, -peak_min*1.5])
+	clean_level = 5.0*sigma
+
 	call(['maths', 'exp=<%s>'%t_restor, 'mask=<%s>.gt.%s'%(t_restor,mask_level), 'out=%s'%t_mask],stdout=logf,stderr=logf)
 	shutil.rmtree(t_restor)
 	shutil.rmtree(t_model)
-	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(clean_level, sigma5), 'region=mask(%s)'%t_mask],stdout=logf,stderr=logf)
+	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=1500', 'cutoff=%s,%s'%(clean_level, 2*sigma), 'region=mask(%s)'%t_mask],stdout=logf,stderr=logf)
 	
-	call(['selfcal', 'vis=%s'%t_pscal, 'model=%s'%t_model, 'interval=0.5', 'nfbin=2', 'options=phase,mfs'],stdout=logf,stderr=logf)
+	call(['selfcal', 'vis=%s'%t_pscal, 'model=%s'%t_model, 'interval=0.5', 'nfbin=1', 'options=phase,mfs'],stdout=logf,stderr=logf)
 	shutil.rmtree(t_map)
 	shutil.rmtree(t_beam)
 	shutil.rmtree(t_mask)
 	shutil.rmtree(t_model)
 
 	t_p2 = t + '.p2.fits'	
-	call(['invert', 'vis=%s'%t_pscal, 'map=%s'%t_map, 'beam=%s'%t_beam, 'robust=0.5', 'stokes=i', 'options=mfs,double,sdb', 'imsize=3,3,beam', 'cell=5,5,res'],stdout=logf,stderr=logf)
-	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(clean_level, sigma5), "region='perc(90)'"],stdout=logf,stderr=logf)
+	call(['invert', 'vis=%s'%t_pscal, 'map=%s'%t_map, 'beam=%s'%t_beam, 'robust=0.5', 'stokes=i', 'options=mfs,double,sdb', 'imsize=2,2,beam', 'cell=5,5,res'],stdout=logf,stderr=logf)
+	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(clean_level, 2*sigma), "region='perc(90)'"],stdout=logf,stderr=logf)
 	call(['restor', 'map=%s'%t_map, 'beam=%s'%t_beam, 'model=%s'%t_model, 'out=%s'%t_restor],stdout=logf,stderr=logf)
 	call(['fits', 'op=xyout', 'in=%s'%t_restor, 'out=%s'%t_p2],stdout=logf,stderr=logf)
-	sigma, peak_flux = get_noise(t_p2)
-	logprint("RMS of p2 image: %s"%sigma, logf)
-	logprint("Peak flux density of p2 image: %s"%peak_flux, logf)
+	sigma, peak_max, peak_min = get_noise(t_p2)
+	# logprint("RMS of p2 image: %s"%sigma, logf)
+	# logprint("Peak flux density of p2 image: %s"%peak_flux, logf)
 
 	# move on to amp selfcal.
 	logprint("***** One round of amp+phase selfcal *****", logf)
-	sigma5 = 5.0*sigma
-	if bool_bright == True:
-		mask_level = 50.0*sigma
-		clean_level = 20.0*sigma
-	else:
-		mask_level = 20.0*sigma
-		clean_level = 10.0*sigma
+	# sigma2 = 2.0*sigma
+	# if bool_bright == True:
+	# 	mask_level = 10.0*sigma
+	# 	clean_level = 3.0*sigma
+	# else:
+	# 	mask_level = 10.0*sigma
+	# 	clean_level = 3.0*sigma
+
+	mask_level = np.amax([10*sigma, -peak_min*1.5])
+	clean_level = 5.0*sigma
+
 	call(['maths', 'exp=<%s>'%t_restor, 'mask=<%s>.gt.%s'%(t_restor,mask_level), 'out=%s'%t_mask],stdout=logf,stderr=logf)
 	shutil.rmtree(t_restor)
 	shutil.rmtree(t_model)
-	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(clean_level, sigma5), 'region=mask(%s)'%t_mask],stdout=logf,stderr=logf)
+	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=1500', 'cutoff=%s,%s'%(clean_level, 2*sigma), 'region=mask(%s)'%t_mask],stdout=logf,stderr=logf)
 	t_ascal = t + '.ascal'
 	call(['uvaver', 'vis=%s'%t_pscal, 'out=%s'%t_ascal],stdout=logf,stderr=logf)
 	
 	# do the first round of amp selfcal with model generated using phase selfcal.
-	call(['selfcal', 'vis=%s'%t_ascal, 'model=%s'%t_model, 'interval=5', 'nfbin=2', 'options=amp,mfs'],stdout=logf,stderr=logf)
+	call(['selfcal', 'vis=%s'%t_ascal, 'model=%s'%t_model, 'interval=5', 'nfbin=1', 'options=amp,mfs'],stdout=logf,stderr=logf)
 	shutil.rmtree(t_map)
 	shutil.rmtree(t_beam)
 	shutil.rmtree(t_mask)
 	shutil.rmtree(t_model)
 
 	t_p2a1 = t + '.p2a1.fits'	
-	call(['invert', 'vis=%s'%t_ascal, 'map=%s'%t_map, 'beam=%s'%t_beam, 'robust=0.5', 'stokes=i', 'options=mfs,double,sdb', 'imsize=3,3,beam', 'cell=5,5,res'],stdout=logf,stderr=logf)
-	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(clean_level, sigma5), "region='perc(90)'"],stdout=logf,stderr=logf)
+	call(['invert', 'vis=%s'%t_ascal, 'map=%s'%t_map, 'beam=%s'%t_beam, 'robust=0.5', 'stokes=i', 'options=mfs,double,sdb', 'imsize=2,2,beam', 'cell=5,5,res'],stdout=logf,stderr=logf)
+	call(['mfclean', 'map=%s'%t_map, 'beam=%s'%t_beam, 'out=%s'%t_model, 'niters=10000', 'cutoff=%s,%s'%(clean_level,2*sigma), "region='perc(90)'"],stdout=logf,stderr=logf)
 	call(['restor', 'map=%s'%t_map, 'beam=%s'%t_beam, 'model=%s'%t_model, 'out=%s'%t_restor],stdout=logf,stderr=logf)
 	call(['fits', 'op=xyout', 'in=%s'%t_restor, 'out=%s'%t_p2a1],stdout=logf,stderr=logf)
-	sigma, peak_flux = get_noise(t_p2a1)
-	logprint("RMS of p2a1 image: %s"%sigma, logf)
-	logprint("Peak flux density of p2a1 image: %s"%peak_flux, logf)
+	# sigma, peak_flux = get_noise(t_p2a1)
+	# logprint("RMS of p2a1 image: %s"%sigma, logf)
+	# logprint("Peak flux density of p2a1 image: %s"%peak_flux, logf)
 	shutil.rmtree(t_map)
 	shutil.rmtree(t_beam)
 	shutil.rmtree(t_restor)
